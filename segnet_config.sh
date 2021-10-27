650814903d29e778b316c2fdff6a3e2efe23e54f
AspirinZJ <zhangmengwei1996@outlook.com>
2021-10-27 16:00:25 -0400

    Added test code

--- segnet_config.sh
+++ segnet_config.sh
@@ -3,8 +3,6 @@
 set -e
 
 sudo apt install git cmake libpython3-dev python3-numpy -y
-cd ~
-cp segnet.py ~
 git clone --recursive git@github.com:dusty-nv/jetson-inference.git
 cd jetson-inference
 mkdir build && cd build
@@ -13,6 +11,7 @@ make -j3
 sudo make install
 sudo ldconfig
 
-cd ~
+cd ../..
 ./segnet.py --network=fcn-resnet18-cityscapes-1024x512 /dev/video0
+./segnet.py --network=fcn-resnet18-cityscapes-1024x512 ./test.jpg ./output.jpg 
 
